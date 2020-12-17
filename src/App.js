0a4c276993172371d953b4a9a639fae040522765
Sohrat <sohrat6128@gmail.com>
2020-12-17 18:11:38 +0300

    create header container

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 
 import './App.css';
+import './container/Header/Header';
+import Header from "./container/Header/Header";
 
 class App extends  Component{
   render(){
     return (
         <div className="App">
-
+            <Header/>
         </div>
     );
   };
