0da2b0355e6d2903faad711dc8e64598fb34c39b
Sohrat <sohrat6128@gmail.com>
2020-12-24 11:29:16 +0300

    create movie list component

---
+++ src/components/MovieList/MovieList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './MovieList.css';
+
+const movieList = props => (
+    <div className='Container'>
+        <p className='Title'>Popular Movies</p>
+        <div className='List-Container'>
+            <div className='Movie-Container'>
+                <img className='Poster-Image' width={175} height={262} src={require('../../assets/poster.jpg').default} alt='image'/>
+                <div className='Movie-Vote-Container'>
+                    <p className='Movie-Star-Icon'>
+                        <i className='fas fa-star'> </i>
+                    </p>
+                    <div>
+                        <p className='Movie-Vote-Average'>8.2</p>
+                    </div>
+                </div>
+                <p className='Movie-Title'>The Mandalorian</p>
+            </div>
+        </div>
+    </div>
+);
+
+export default movieList;
