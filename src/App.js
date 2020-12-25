0e5233818aefa8e889eede1d49b4394d854b227a
Sohrat <sohrat6128@gmail.com>
2020-12-25 23:13:32 +0300

    create detail container

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from "./containers/Header/Header";
 import Home from './containers/Home/Home';
+import Detail from './containers/Detail/Detail';
 
 class App extends  Component{
   render(){
     return (
         <div className="App">
             <Header/>
-            <Home/>
+            {/*<Home/>*/}
+            <Detail/>
         </div>
     );
   };
