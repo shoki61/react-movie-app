3321a74c70fbef5faebb7960742354fd45aa1c26
Sohrat <sohrat6128@gmail.com>
2020-12-25 21:47:10 +0300

    fix css

--- src/components/Navigations/Navigations.js
+++ src/components/Navigations/Navigations.js
@@ -6,14 +6,16 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigations = () => (
     <div className='Navigation'>
         <NavigationItem title='Movies'>
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
+            <a href="#">Popular</a>
+            <a href="#">Now Playing</a>
+            <a href="#">Upcoming</a>
+            <a href="#">Top Rated</a>
         </NavigationItem>
-        <NavigationItem title="TV's">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
+        <NavigationItem title="TV">
+            <a href="#">Popular</a>
+            <a href="#">Airing Today</a>
+            <a href="#">On Today</a>
+            <a href="#">Top Rated</a>
         </NavigationItem>
         <NavigationItem title='Genres'>
             <a href="#">Link 1</a>
