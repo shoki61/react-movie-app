6b8f8af37b7d67517feb6ecac58818d4f8f833ed
Sohrat <sohrat6128@gmail.com>
2021-01-05 13:22:31 +0300

    set popular movies action

---
+++ src/store/actions/movie.js
@@ -0,0 +1,42 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios';
+import { APIKey } from '../../env';
+
+
+const popularMoviesStart = () => {
+    return{
+        type: actionTypes.GET_POPULAR_MOVIES_START
+    };
+};
+
+const popularMoviesSuccess = data => {
+    return {
+        type: actionTypes.GET_POPULAR_MOVIES_SUCCESS,
+        popularMovies: data
+    };
+};
+
+const popularMoviesFail = () => {
+    return {
+        type:actionTypes.GET_POPULAR_MOVIES_FAIL
+    };
+};
+
+
+const getPopularMovies = () => {
+    return dispatch => {
+        dispatch(popularMoviesStart());
+        axios.get(`/movie/popular?api_key=${APIKey}&language=en-US&page=1`)
+            .then(response=>{
+                console.log(response.data)
+                dispatch(popularMoviesSuccess(response))
+            })
+            .catch(error=>{
+                dispatch(popularMoviesFail())
+            });
+    };
+};
+
+export {
+    getPopularMovies
+}
