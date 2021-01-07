0f70c83682b2f83fd6a60b927624bf425d48526f
Sohrat <sohrat6128@gmail.com>
2021-01-07 15:41:27 +0300

    set credits in list component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -7,39 +7,21 @@ const list = props => (
     <div className='List'>
         <p className='List-Title'>{props.title}<span className='List-Count'>({props.count})</span></p>
         <div className='List-Items-Container'>
-            <div onClick={()=> props.clicked()} className='List-Item-Container'>
-                <Image
-                    src={props.src}
-                    alt='image'
-                    className={['List-Image',props.override].join(' ')}
-                />
-                <div>
-                    <p className='List-Name'>{props.name}</p>
-                    <p className='List-who'>{props.who}</p>
-                </div>
-            </div>
-            <div className='List-Item-Container'>
-                <Image
-                    src={props.src}
-                    alt='image'
-                    className={['List-Image',props.override].join(' ')}
-                />
-                <div>
-                    <p className='List-Name'>{props.name}</p>
-                    <p className='List-who'>{props.who}</p>
-                </div>
-            </div>
-            <div className='List-Item-Container'>
-                <Image
-                    src={props.src}
-                    alt='image'
-                    className={['List-Image',props.override].join(' ')}
-                />
-                <div>
-                    <p className='List-Name'>{props.name}</p>
-                    <p className='List-who'>{props.who}</p>
-                </div>
-            </div>
+            {
+                props.data.map(item => (
+                    <div onClick={()=> props.clicked()} className='List-Item-Container'>
+                        <Image
+                            src={`https://image.tmdb.org/t/p/w500${item.profile_path}`}
+                            alt='image'
+                            className={['List-Image',props.override].join(' ')}
+                        />
+                        <div>
+                            <p className='List-Name'>{item.name}</p>
+                            <p className='List-who'>{item.known_for_department}</p>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     </div>
 );
--- src/containers/Detail/Detail.js
+++ src/containers/Detail/Detail.js
@@ -10,6 +10,7 @@ import Image from '../../components/UI/Image/Image';
 import Link from '../../components/UI/Link/Link';
 import List from '../../components/List/List';
 import languagesData from '../../data/languages.json';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Detail extends Component {
 
@@ -20,120 +21,124 @@ class Detail extends Component {
     render() {
         return (
             <div className='Detail'>
-                <div className='Detail-Top'>
-                    <div className='Detail-Left'>
-                        <Image
-                            className='Detail-Poster'
-                            alt='image'
-                            src={`https://image.tmdb.org/t/p/w500${this.props.movieDetail.poster_path}`}
-                        />
-                    </div>
-                    <div className='Detail-Right'>
-                        <p>{JSON.stringify(this.props.movieDetail)}</p>
-                        <div className='Name-Container'>
-                            <div>
-                                <p className='Name'>{this.props.movieDetail.title}</p>
-                                <p className='Detail-Tag-Line'>{this.props.movieDetail.tagline}</p>
+                {
+                    this.props.movieDetail && this.props.credits
+                        ?<div>
+                            <div className='Detail-Top'>
+                                <div className='Detail-Left'>
+                                    <Image
+                                        className='Detail-Poster'
+                                        alt='image'
+                                        src={`https://image.tmdb.org/t/p/w500${this.props.movieDetail.poster_path}`}
+                                    />
+                                </div>
+                                <div className='Detail-Right'>
+                                    <div className='Name-Container'>
+                                        <div>
+                                            <p className='Name'>{this.props.movieDetail.title}</p>
+                                            <p className='Detail-Tag-Line'>{this.props.movieDetail.tagline}</p>
+                                        </div>
+                                        <span className='Detail-Release-Date'>{this.props.movieDetail.release_date} / <AiFillPlayCircle style={{fontSize:11}}/> {this.props.movieDetail.runtime} min</span>
+                                    </div>
+                                    <div className='Detail-Average'>
+                                        <i className='fas fa-star'/>{this.props.movieDetail.vote_average} <div className='Detail-Average-Count'>/{this.props.movieDetail.vote_count}</div>
+                                    </div>
+                                    <ul>
+                                        {
+                                            this.props.movieDetail.genres.map(genre => (
+                                                <li key={genre.id} onClick={()=> this.goGenre(genre.name)}>{genre.name}<span>,</span></li>
+                                            ))
+                                        }
+                                    </ul>
+                                    <div>
+                                        <p className='Detail-Title'>Overview</p>
+                                        <p className='Detail-Overview'>
+                                            {this.props.movieDetail.overview}
+                                        </p>
+                                    </div>
+                                    <div style={{display:'flex',width:'50%',justifyContent:'space-between'}}>
+                                        <div>
+                                            <p className='Detail-Title'>Original Language</p>
+                                            <p className='Detail-Info'>{languagesData.languages.filter(item => item.iso_639_1 === this.props.movieDetail.original_language)[0].english_name}</p>
+                                        </div>
+                                        <div>
+                                            <p className='Detail-Title'>Budged</p>
+                                            <p className='Detail-Info'>${this.props.movieDetail.budget}</p>
+                                        </div>
+                                        <div>
+                                            <p className='Detail-Title'>Revenue</p>
+                                            <p className='Detail-Info'>${this.props.movieDetail.revenue}</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <p className='Detail-Title'>Director</p>
+                                        <p className='Detail-Info'>Christopher Nolan</p>
+                                    </div>
+                                    <div className='External-Ids-Container'>
+                                        <div className='External-Id' title='Trailer'>
+                                            <Link >
+                                                <BsPlayFill/>
+                                            </Link>
+                                        </div>
+                                        <div className='External-Id' title='Website'>
+                                            <Link >
+                                                <AiOutlineLink/>
+                                            </Link>
+                                        </div>
+                                        <div className='External-Id' title='IMDb'>
+                                            <Link url={`https://www.imdb.com/title/${this.props.movieDetail.imdb_id}`}>
+                                                <SiImdb/>
+                                            </Link>
+                                        </div>
+                                        <div className='External-Id' title='Facebook'>
+                                            <Link >
+                                                <ImFacebook/>
+                                            </Link>
+                                        </div>
+                                        <div className='External-Id' title='Instagram'>
+                                            <Link >
+                                                <AiFillInstagram/>
+                                            </Link>
+                                        </div>
+                                        <div className='External-Id' title='Twitter'>
+                                            <Link >
+                                                <AiOutlineTwitter/>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <span className='Detail-Release-Date'>{this.props.movieDetail.release_date} / <AiFillPlayCircle style={{fontSize:11}}/> {this.props.movieDetail.runtime} min</span>
+                            <div className='Detail-Bottom'>
+                                <div>
+                                    <List
+                                        clicked={this.goPersonalInformation}
+                                        title='Cast'
+                                        count={this.props.credits.cast.length}
+                                        data={this.props.credits.cast}
+                                    />
+                                </div>
+                                {/*<div>*/}
+                                {/*    <List*/}
+                                {/*        title='Crew'*/}
+                                {/*        count={16}*/}
+                                {/*        src={require('../../assets/christopherNolan.jpg').default}*/}
+                                {/*        name='Christopher Nolan'*/}
+                                {/*        who='Director, Writer'*/}
+                                {/*    />*/}
+                                {/*</div>*/}
+                                {/*<div>*/}
+                                {/*    <List*/}
+                                {/*        title='Production Companies'*/}
+                                {/*        count={8}*/}
+                                {/*        src={require('../../assets/christopherNolan.jpg').default}*/}
+                                {/*        name='Syncopy'*/}
+                                {/*    />*/}
+                                {/*</div>*/}
+                            </div>
                         </div>
-                        <div className='Detail-Average'>
-                            <i className='fas fa-star'/>{this.props.movieDetail.vote_average} <div className='Detail-Average-Count'>/{this.props.movieDetail.vote_count}</div>
-                        </div>
-                        <ul>
-                            {
-                                this.props.movieDetail.genres.map(genre => (
-                                    <li key={genre.id} onClick={()=> this.goGenre(genre.name)}>{genre.name}<span>,</span></li>
-                                ))
-                            }
-                        </ul>
-                        <div>
-                            <p className='Detail-Title'>Overview</p>
-                            <p className='Detail-Overview'>
-                                {this.props.movieDetail.overview}
-                            </p>
-                        </div>
-                        <div style={{display:'flex',width:'50%',justifyContent:'space-between'}}>
-                            <div>
-                                <p className='Detail-Title'>Original Language</p>
-                                <p className='Detail-Info'>{languagesData.languages.filter(item => item.iso_639_1 === this.props.movieDetail.original_language)[0].english_name}</p>
-                            </div>
-                            <div>
-                                <p className='Detail-Title'>Budged</p>
-                                <p className='Detail-Info'>${this.props.movieDetail.budget}</p>
-                            </div>
-                            <div>
-                                <p className='Detail-Title'>Revenue</p>
-                                <p className='Detail-Info'>${this.props.movieDetail.revenue}</p>
-                            </div>
-                        </div>
-                        <div>
-                            <p className='Detail-Title'>Director</p>
-                            <p className='Detail-Info'>Christopher Nolan</p>
-                        </div>
-                        <div className='External-Ids-Container'>
-                            <div className='External-Id' title='Trailer'>
-                                <Link >
-                                    <BsPlayFill/>
-                                </Link>
-                            </div>
-                            <div className='External-Id' title='Website'>
-                                <Link >
-                                    <AiOutlineLink/>
-                                </Link>
-                            </div>
-                            <div className='External-Id' title='IMDb'>
-                                <Link url={`https://www.imdb.com/title/${this.props.movieDetail.imdb_id}`}>
-                                    <SiImdb/>
-                                </Link>
-                            </div>
-                            <div className='External-Id' title='Facebook'>
-                                <Link >
-                                    <ImFacebook/>
-                                </Link>
-                            </div>
-                            <div className='External-Id' title='Instagram'>
-                                <Link >
-                                    <AiFillInstagram/>
-                                </Link>
-                            </div>
-                            <div className='External-Id' title='Twitter'>
-                                <Link >
-                                    <AiOutlineTwitter/>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='Detail-Bottom'>
-                    <div>
-                        <List
-                            clicked={this.goPersonalInformation}
-                            title='Cast'
-                            count={45}
-                            src={require('../../assets/christopherNolan.jpg').default}
-                            name='Christopher Nolan'
-                            who='Director, Writer'
-                        />
-                    </div>
-                    <div>
-                        <List
-                            title='Crew'
-                            count={16}
-                            src={require('../../assets/christopherNolan.jpg').default}
-                            name='Christopher Nolan'
-                            who='Director, Writer'
-                        />
-                    </div>
-                    <div>
-                        <List
-                            title='Production Companies'
-                            count={8}
-                            src={require('../../assets/christopherNolan.jpg').default}
-                            name='Syncopy'
-                        />
-                    </div>
-                </div>
+                        :<Spinner/>
+
+                }
             </div>
         );
     };
@@ -141,7 +146,9 @@ class Detail extends Component {
 
 const mapStateToProps = state => {
     return{
-        movieDetail: state.movie.movieDetail
+        movieDetail: state.movie.movieDetail,
+        credits: state.movie.credits,
+        externalID: state.movie.externalID
     };
 };
 
