804600ff3760b53a870264dcb33ee11e2133906f
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-04-23 15:46:08 +0300

    add redirect to app logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Logo.css';
 
 const logo = () => (
-    <div className='Logo'>
+    <Link to='/' className='Logo'>
         <img src={require('../../assets/logo.png').default} alt='logo'/>
         <div>
             <p className='Logo-Name'>SHOKIDEV</p>
             <p className='App-Name'>movie</p>
         </div>
-    </div>
+    </Link>
 );
 
 export default logo;
